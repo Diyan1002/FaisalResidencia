c7ef3cab64e58f573567d16c1a37bf60fdbaf05c
Muhammad Diyan Irfan <diyanirfan56@gmail.com>
2025-12-04 23:15:05 +0500

    cs

--- src/Components/SocietyBanner.jsx
+++ src/Components/SocietyBanner.jsx
@@ -3,11 +3,11 @@ import banner from "../assets/ban.png";
 
 export default function SocietyBanner() {
   return (
-    <div className="w-full h-[260px] sm:h-[280px] md:h-[400px] lg:h-[520px] xl:h-[600px] overflow-hidden">
+    <div className="w-full">
       <img 
         src={banner} 
         alt="Faisal Residencia E-17 Islamabad" 
-        className="w-full h-full "
+        className="w-full object-cover"
       />
     </div>
   );
